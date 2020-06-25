bdf166fdbf3d4afa7425e765f50871eea15b1ca8
ZMarina Monteiro <zmarina.hm@gmail.com>
2020-06-25 03:06:48 -0300

    dynamic info text modal

--- scripts.js
+++ scripts.js
@@ -7,9 +7,12 @@ for (let card of cards){
 
     card.addEventListener("click", function (){
         const fotoId = card.getAttribute("id");
+        const title = card.querySelector(".content").innerHTML;
+        const text = card.querySelector(".info").innerHTML;
         modalOverlay.classList.add("active");
         modalOverlay.querySelector("img").src=`https://images.unsplash.com/photo-${fotoId}?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80`;
-        
+        modalOverlay.querySelector(".title").innerHTML = title;
+        modalOverlay.querySelector(".info").innerHTML = text; 
     })
 }
 
